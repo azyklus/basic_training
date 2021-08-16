c1629bd8053df4efc9746fd71519c0158744576a
Yarot Kell <mechild02@gmail.com>
2021-08-16 01:17:53 -0400

    [IN] added license and readme files and Java examples.

---
+++ examples/java/hello_world/Main.java
@@ -0,0 +1,11 @@
+// https://github.com/mnimi/intro_to_programming/tree/trunk/examples/java/hello_world/Main.java
+
+package me.yarot.ProgrammingBasics;
+
+public class Application
+{
+   public static void Main()
+   {
+      System.out.println("Hello, world!");
+   }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Intro to Programming
+
+A detailed tutorial of important details in software.
+*Written by Mnimi*.
